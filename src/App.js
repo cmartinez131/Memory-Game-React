77f5529d1ea4dc307f20def1647bc071e35aed13
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-03-25 17:33:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function Board() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="title">Memory Game</div>
+    <div className="status">game status: </div>
+    <Square />
+
+    </>
   );
 }
-
-export default App;
+function Square() {
+  return <button className="square">Image</button>
+}
