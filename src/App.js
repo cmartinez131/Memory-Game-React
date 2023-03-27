bc03d5e8bd062213f66ef6b1d7b81896389f15f8
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-03-27 16:33:57 -0400

    moved some functionality to game component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import { useState } from 'react'
 
-function Card({ value , onCardClick , choice1, choice2}) {
-  return <button className="card" onClick={onCardClick}>{value}</button>
+export default function Game() {
+  //todo: hold state of game
+  const [numChoices, setNumChoices] = useState(0);
+  const [numTurns, setNumTurns] = useState(0);
+  const [firstCard, setFirstCard] = useState(null);
+  const [secondCard, setSecondCard] = useState(null);
+
+  const status = ""
+
+  function handlePlay(card) {
+    if (numChoices == 0){
+      setNumChoices(numChoices + 1);//we made one choice
+      setFirstCard(card);
+      console.log(card)
+    } 
+    else if (numChoices == 1) {//we are making our second choice
+      setNumChoices(numChoices + 1)
+      setSecondCard(card);
+      checkMatch();
+      setNumChoices(0);
+      setNumTurns(numTurns+1);
+      console.log(card);
+      //check if match
+    }
+    if (numChoices == 2) {
+      checkMatch()
+      setNumChoices(0)
+    }
+  }
+  
+  
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board numChoices={numChoices} onPlay={handlePlay}/>
+      </div>
+      <div className='game-info'>
+        <div>{status}</div>
+        <h2>Number of turns: {numTurns}</h2>
+      </div>
+    </div>
+  );
 }
 
-function Board() {
+function Board({ numChoices, onPlay }) {
   //todo: create a function to shuffle the board and start new game
   const cardsData = [
     { id: 1, frontImage: 'card1', backImage: "--", isFacedDown: true, isMatched: false},
@@ -27,7 +68,6 @@ function Board() {
   function handleClick(i) {
     const nextCards = cards.slice();
     const cardClicked = nextCards[i]
-    
     //two cases: 1. we are picking the second card
     //           2. we are picking the first card
 //only does function if we have no cards picked yet, and the card picked is faced down and not matched
@@ -37,36 +77,44 @@ function Board() {
         cardClicked.isFacedDown = false
         setChoice2(cardClicked.id)
         console.log(choice1, cardClicked.id)
+        setCards(nextCards);  //update the cards to reflect picked card
+        onPlay(cardClicked);
       }
     }
     if (!choice1){  //if we dont have choice 1, pick choice1 and set choice 1 only if the card is faced down and not matched
       console.log(cardClicked.id)
+
       if (cardClicked.isFacedDown && cardClicked.isMatched === false) {
         [cardClicked.backImage, cardClicked.frontImage] = [cardClicked.frontImage, cardClicked.backImage]
         cardClicked.isFacedDown = false
         setChoice1(cardClicked.id)
+        setCards(nextCards);  //update the cards to reflect picked card
+        onPlay(cardClicked);
       }
     }
-    setCards(nextCards);  //update the cards to reflect picked card
-    //TODO
-    //toggle handleTurn which will take choice1 and choice 2 and manipulate the cards array
-          
+  }
+
+  const match = checkMatch(cards);
+  let status = "Game Started";
+  if (match) {
+    status = "Match!"
   }
   return (
     <>
+    <button className="start-button">Start New Game</button>
     <div className="title">Memory Game</div>
-    <div className="status">game status: </div>
+    <div className="status">game status: {status} </div>
     <div className="board-row">
-      <Card value={cards[0].backImage} onCardClick={() => handleClick(0)} choice1={choice1} choice2={choice2} />
-      <Card value={cards[1].backImage} onCardClick={() => handleClick(1)} choice1={choice1} choice2={choice2} />
-      <Card value={cards[2].backImage} onCardClick={() => handleClick(2)} choice1={choice1} choice2={choice2} />
-      <Card value={cards[3].backImage} onCardClick={() => handleClick(3)} choice1={choice1} choice2={choice2} />
+      <Card value={cards[0].backImage} onCardClick={() => handleClick(0)} />
+      <Card value={cards[1].backImage} onCardClick={() => handleClick(1)} />
+      <Card value={cards[2].backImage} onCardClick={() => handleClick(2)} />
+      <Card value={cards[3].backImage} onCardClick={() => handleClick(3)} />
     </div>
     <div className="board-row">
-      <Card value={cards[4].backImage} onCardClick={() => handleClick(4)} choice1={choice1} choice2={choice2} />
-      <Card value={cards[5].backImage} onCardClick={() => handleClick(5)} choice1={choice1} choice2={choice2} />
-      <Card value={cards[6].backImage} onCardClick={() => handleClick(6)} choice1={choice1} choice2={choice2} />
-      <Card value={cards[7].backImage} onCardClick={() => handleClick(7)} choice1={choice1} choice2={choice2} />
+      <Card value={cards[4].backImage} onCardClick={() => handleClick(4)} />
+      <Card value={cards[5].backImage} onCardClick={() => handleClick(5)} />
+      <Card value={cards[6].backImage} onCardClick={() => handleClick(6)} />
+      <Card value={cards[7].backImage} onCardClick={() => handleClick(7)} />
     </div>
     <div className="board-row">
       <Card value={cards[8].backImage} onCardClick={() => handleClick(8)} choice1={choice1} choice2={choice2} />
@@ -78,19 +126,9 @@ function Board() {
   );
 }
 
-export default function Game() {
-  return (
-    <div className='game'>
-      <div className='game-board'>
-        <Board />
-      </div>
-      <div className='game-info'>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
-  );
+function Card({ value , onCardClick , choice1, choice2}) {
+  return <button className="card" onClick={onCardClick}>{value}</button>
 }
-
 
 function calculateWinner(cards) {
   for (let i = 0; i < cards.length; i++){
@@ -101,3 +139,6 @@ function calculateWinner(cards) {
   return true;
 }
 
+function checkMatch(card1,card2) {
+  return (card1 == card2)
+}
