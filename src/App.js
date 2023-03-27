05b5be5734abf731dd8348cf60c4fae773fade97
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-03-27 12:23:46 -0400

    cleanup handleClick function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function Card({ value , onCardClick , choice1, choice2}) {
 }
 
 export default function Board() {
+  //todo: create a function to shuffle the board and start new game
   const cardsData = [
     { id: 1, frontImage: 'card1', backImage: "--", isFacedDown: true, isMatched: false},
     { id: 1, frontImage: 'card1', backImage: "--", isFacedDown: true, isMatched: false},
@@ -14,31 +15,42 @@ export default function Board() {
     { id: 3, frontImage: 'card3', backImage: "--", isFacedDown: true, isMatched: false},
     { id: 4, frontImage: 'card4', backImage: "--", isFacedDown: true, isMatched: false},
     { id: 4, frontImage: 'card4', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 5, frontImage: 'card3', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 5, frontImage: 'card3', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 6, frontImage: 'card4', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 6, frontImage: 'card4', backImage: "--", isFacedDown: true, isMatched: false},
+    { id: 5, frontImage: 'card5', backImage: "--", isFacedDown: true, isMatched: false},
+    { id: 5, frontImage: 'card5', backImage: "--", isFacedDown: true, isMatched: false},
+    { id: 6, frontImage: 'card6', backImage: "--", isFacedDown: true, isMatched: false},
+    { id: 6, frontImage: 'card6', backImage: "--", isFacedDown: true, isMatched: false},
   ]
   const [cards, setCards] = useState(cardsData);
-  const [choice1, setChoice1] = useState(false)
-  const [choice2, setChoice2] = useState(false)
+  const [choice1, setChoice1] = useState(null)
+  const [choice2, setChoice2] = useState(null)
 
   function handleClick(i) {
     const nextCards = cards.slice();
-    if (nextCards[i].isFacedDown && nextCards[i].isMatched === false) {
-      [nextCards[i].backImage, nextCards[i].frontImage] = [nextCards[i].frontImage, nextCards[i].backImage]
-      nextCards[i].isFacedDown = false
+    const cardClicked = nextCards[i]
+    
+    //two cases: 1. we are picking the second card
+    //           2. we are picking the first card
+//only does function if we have no cards picked yet, and the card picked is faced down and not matched
+    if (choice1){
+      if (cardClicked.isFacedDown && cardClicked.isMatched == false){
+        [cardClicked.backImage, cardClicked.frontImage] = [cardClicked.frontImage, cardClicked.backImage]
+        cardClicked.isFacedDown = false
+        setChoice2(cardClicked.id)
+        console.log(choice1, cardClicked.id)
+      }
     }
-     // cards start face down
-     //can only flip cards if it is faced down
-     //flip the card on its back
-     // todo: logic for matching cards
-
-     //see how many choices we have
-     //if we have no choices yet, the user picks a card, update choice 1
-     //if we have one choice, the user picks a card, updates choice 2
-     //if choice1 and choice2 match, set both cards isMatched value to True
-    setCards(nextCards);
+    if (!choice1){  //if we dont have choice 1, pick choice1 and set choice 1 only if the card is faced down and not matched
+      console.log(cardClicked.id)
+      if (cardClicked.isFacedDown && cardClicked.isMatched == false) {
+        [cardClicked.backImage, cardClicked.frontImage] = [cardClicked.frontImage, cardClicked.backImage]
+        cardClicked.isFacedDown = false
+        setChoice1(cardClicked.id)
+      }
+    }
+    setCards(nextCards);  //update the cards to reflect picked card
+    //TODO
+    //toggle handleTurn which will take choice1 and choice 2 and manipulate the cards array
+          
   }
   return (
     <>
@@ -57,10 +69,10 @@ export default function Board() {
       <Card value={cards[7].backImage} onCardClick={() => handleClick(7)} choice1={choice1} choice2={choice2} />
     </div>
     <div className="board-row">
-      <Card value={cards[8].backImage} onCardClick={() => handleClick(8)} />
-      <Card value={cards[9].backImage} onCardClick={() => handleClick(9)} />
-      <Card value={cards[10].backImage} onCardClick={() => handleClick(10)} />
-      <Card value={cards[11].backImage} onCardClick={() => handleClick(11)} />
+      <Card value={cards[8].backImage} onCardClick={() => handleClick(8)} choice1={choice1} choice2={choice2} />
+      <Card value={cards[9].backImage} onCardClick={() => handleClick(9)} choice1={choice1} choice2={choice2} />
+      <Card value={cards[10].backImage} onCardClick={() => handleClick(10)} choice1={choice1} choice2={choice2} />
+      <Card value={cards[11].backImage} onCardClick={() => handleClick(11)} choice1={choice1} choice2={choice2} />
     </div>
     </>
   );
