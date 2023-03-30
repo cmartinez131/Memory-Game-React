4ccc0691b40db521b6a59616d75dba5560860b44
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-03-29 21:26:37 -0400

    use team logo as images for cards

--- src/App.js
+++ src/App.js
@@ -1,80 +1,74 @@
 import { useState } from 'react'
 
+
+
 export default function Board() {//{ numChoices, onPlay }
   //todo: create a function to shuffle the board and start new game
   let cardsData = [
-    { id: 1, frontImage: 'card1', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 1, frontImage: 'card1', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 2, frontImage: 'card2', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 2, frontImage: 'card2', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 3, frontImage: 'card3', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 3, frontImage: 'card3', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 4, frontImage: 'card4', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 4, frontImage: 'card4', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 5, frontImage: 'card5', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 5, frontImage: 'card5', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 6, frontImage: 'card6', backImage: "--", isFacedDown: true, isMatched: false},
-    { id: 6, frontImage: 'card6', backImage: "--", isFacedDown: true, isMatched: false},
+    { frontImage: '/images/cubs.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/cubs.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/giants.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/giants.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/dodgers.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/dodgers.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/mets.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/mets.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/yankees.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/yankees.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/redsox.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/redsox.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
   ]
-  let gameIsOver = false
-  //Array(12).fill(null)
+  let gameFinished = false;
   const [cards, setCards] = useState(cardsData);
-  
   const [choice1, setChoice1] = useState(null)
   const [choice2, setChoice2] = useState(null)
   const [numTurns, setNumTurns] = useState(0);
-  //shuffle 
-  
-  
-  function shuffle(cardsArray) {
-    console.log("hi")
-  }
-  
 
   function onStartClick() {
-    console.log("new game button Pressed")
+    console.log("new game button Pressed");
+  }
+
+  function checkMatch(card1,card2) {
+    return (card1 === card2)
+  }
+
+  function calculateWinner(cards) {
+    for (let i = 0; i < cards.length; i++){
+      if (cards.isMatched === false) {
+        return false; //game is not over yet because we have a card that isn't matched
+      }
+    }
+    return true;
   }
 
   function handleClick(i) {
     const nextCards = cards.slice();
     const cardClicked = nextCards[i]
-//only does function if we have no cards picked yet, and the card picked is faced down and not matched
     if (choice1){
       if (cardClicked.isFacedDown && cardClicked.isMatched === false){
-        [cardClicked.backImage, cardClicked.frontImage] = [cardClicked.frontImage, cardClicked.backImage]
-        cardClicked.isFacedDown = false
-        setChoice2(cardClicked.id)
-        console.log(choice1, cardClicked.id)
+        setChoice2(cardClicked.frontImage);
+        console.log("choice 1 is ", choice1, " and choice 2 is: ", cardClicked.frontImage);
+      
+        [cardClicked.backImage, cardClicked.frontImage] = [cardClicked.frontImage, cardClicked.backImage];
+        cardClicked.isFacedDown = false;
         setCards(nextCards);  //update the cards to reflect picked card
         setNumTurns(numTurns+1);
-        if (checkMatch(choice1,choice2)) {
-          status = "Match!"
-        }
-
+        
+        checkMatch(choice1, cardClicked.frontImage);
         //use setTimeout() here
         setChoice1(null);
         setChoice2(null);
       }
+      checkMatch(choice1, cardClicked.frontImage);
     }
     if (!choice1){  //if we dont have choice 1, pick choice1 and set choice 1 only if the card is faced down and not matched
-      console.log(cardClicked.id)
-
       if (cardClicked.isFacedDown && cardClicked.isMatched === false) {
-        [cardClicked.backImage, cardClicked.frontImage] = [cardClicked.frontImage, cardClicked.backImage]
-        cardClicked.isFacedDown = false
-        setChoice1(cardClicked.id)
+        setChoice1(cardClicked.frontImage); //set choice 1
+        [cardClicked.backImage, cardClicked.frontImage] = [cardClicked.frontImage, cardClicked.backImage] //flip card
+        cardClicked.isFacedDown = false;  //change card to be faced up
         setCards(nextCards);  //update the cards to reflect picked card
-        if (checkMatch(choice1,choice2)) {
-          status = "Match!"
-        }
       }
     }
-  }
-
-  const match = checkMatch(cards);
-  let status = "Game Started";
-  if (match) {
-    status = "Match!"
   }
   return (
     <>
@@ -83,48 +77,40 @@ export default function Board() {//{ numChoices, onPlay }
       <button onClick={onStartClick}>Start New Game</button>
     </div>
     <div className="board-row">
-      <Card value={cards[0].backImage} onCardClick={() => handleClick(0)} />
-      <Card value={cards[1].backImage} onCardClick={() => handleClick(1)} />
-      <Card value={cards[2].backImage} onCardClick={() => handleClick(2)} />
-      <Card value={cards[3].backImage} onCardClick={() => handleClick(3)} />
+      <Card frontImage={cards[0].frontImage} backImage={cards[0].backImage} isFacedDown={cards[0].isFacedDown} onCardClick={() => handleClick(0)} />
+      <Card frontImage={cards[1].frontImage} backImage={cards[1].backImage} isFacedDown={cards[1].isFacedDown} onCardClick={() => handleClick(1)} />
+      <Card frontImage={cards[2].frontImage} backImage={cards[2].backImage} isFacedDown={cards[2].isFacedDown} onCardClick={() => handleClick(2)} />
+      <Card frontImage={cards[3].frontImage} backImage={cards[3].backImage} isFacedDown={cards[3].isFacedDown} onCardClick={() => handleClick(3)} />
     </div>
     <div className="board-row">
-      <Card value={cards[4].backImage} onCardClick={() => handleClick(4)} />
-      <Card value={cards[5].backImage} onCardClick={() => handleClick(5)} />
-      <Card value={cards[6].backImage} onCardClick={() => handleClick(6)} />
-      <Card value={cards[7].backImage} onCardClick={() => handleClick(7)} />
+      <Card frontImage={cards[4].frontImage} backImage={cards[4].backImage} isFacedDown={cards[4].isFacedDown} onCardClick={() => handleClick(4)} />
+      <Card frontImage={cards[5].frontImage} backImage={cards[5].backImage} isFacedDown={cards[5].isFacedDown} onCardClick={() => handleClick(5)} />
+      <Card frontImage={cards[6].frontImage} backImage={cards[6].backImage} isFacedDown={cards[6].isFacedDown} onCardClick={() => handleClick(6)} />
+      <Card frontImage={cards[7].frontImage} backImage={cards[7].backImage} isFacedDown={cards[7].isFacedDown} onCardClick={() => handleClick(7)} />
     </div>
     <div className="board-row">
-      <Card value={cards[8].backImage} onCardClick={() => handleClick(8)} />
-      <Card value={cards[9].backImage} onCardClick={() => handleClick(9)} />
-      <Card value={cards[10].backImage} onCardClick={() => handleClick(10)} />
-      <Card value={cards[11].backImage} onCardClick={() => handleClick(11)} />
+      <Card frontImage={cards[8].frontImage} backImage={cards[8].backImage} isFacedDown={cards[8].isFacedDown} onCardClick={() => handleClick(8)} />
+      <Card frontImage={cards[9].frontImage} backImage={cards[9].backImage} isFacedDown={cards[9].isFacedDown} onCardClick={() => handleClick(9)} />
+      <Card frontImage={cards[10].frontImage} backImage={cards[10].backImage} isFacedDown={cards[10].isFacedDown} onCardClick={() => handleClick(10)} />
+      <Card frontImage={cards[11].frontImage} backImage={cards[11].backImage} isFacedDown={cards[11].isFacedDown} onCardClick={() => handleClick(11)} />
     </div>
     <div className="status">Number of turns: {numTurns} </div>
     </>
   );
 }
 
-function Card({ value , onCardClick}) {
+function Card({ frontImage, backImage, isFacedDown, onCardClick }) {
+  let imageSource = ""
+  imageSource = backImage
   return (
-      <img 
-        className="card-back" 
-        src={"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Major_League_Baseball_logo.svg/1200px-Major_League_Baseball_logo.svg.png"} 
-        onClick={onCardClick}
-      />
+    <img 
+      className="card" 
+      src={imageSource} 
+      alt="mlb logo"
+      onClick={onCardClick}
+    />
   )
-
 }
 
-function calculateWinner(cards) {
-  for (let i = 0; i < cards.length; i++){
-    if (cards.isMatched === false) {
-      return false; //game is not over yet because we have a card that isn't matched
-    }
-  }
-  return true;
-}
 
-function checkMatch(card1,card2) {
-  return (card1 === card2)
-}
+
