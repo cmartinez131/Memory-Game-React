4e9cbd5334daa0a8adf8c7099a777422e0f6cf69
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-03-31 15:14:18 -0400

    Revert "Merge pull request #13 from cmartinez131/main"
    This reverts commit 37ed513a7edca381b1873b5a34559aefeb9acb68, reversing
    changes made to 4ccc0691b40db521b6a59616d75dba5560860b44.

---
+++ README.md
@@ -0,0 +1,91 @@
+Memory Game using React
+
+The link to the deployed website using GitHub Pages: https://cmartinez131.github.io/Memory-Game-React/
+
+Used Git Feature Branch Workflow https://www.atlassian.com/git/tutorials/comparing-workflows/feature-branch-workflow
+
+
+Requiremen:
+At least 12 cards are displayed initially face down. Maximum of 30 cards. 
+There should be a theme to the cards and not just random images.
+The cards should all be the same dimension. (grid should not look messy)
+Should have at least 3 rows of cards.
+There cannot be duplicate paired cards. (For example, you cannot have 4 cards that are ace of spades)
+These cards should be shuffled upon pressing a button called “New Game”
+User should be able to reset the game at any point with the "New Game" button
+The user can only select two cards per turn.
+If those selections match, those cards stay upright (can also have them become transparent or other styling choices indicating they are matched)
+If the selections do not match, there should be a delay and then the cards should turn face down again
+While the cards are being compared, other card selections should be disabled
+Should be deployed on Github Pages.
+No design frameworks allowed! Includes but is not limited to Bootstrap, Material-UI, Foundation, etc. Please email me if you are concerned that your approach may break this rule)
+Must use useState hook for handling the state of the game.
+Should contain a live counter for how many turns made so far.
+Announce when the game is completed.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+
+If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+
+You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### Analyzing the Bundle Size
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### Advanced Configuration
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `npm run build` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
---
+++ src/App.js
@@ -0,0 +1,116 @@
+import { useState } from 'react'
+
+
+
+export default function Board() {//{ numChoices, onPlay }
+  //todo: create a function to shuffle the board and start new game
+  let cardsData = [
+    { frontImage: '/images/cubs.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/cubs.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/giants.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/giants.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/dodgers.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/dodgers.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/mets.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/mets.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/yankees.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/yankees.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/redsox.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+    { frontImage: '/images/redsox.png', backImage: '/images/mlb.png', isFacedDown: true, isMatched: false},
+  ]
+  let gameFinished = false;
+  const [cards, setCards] = useState(cardsData);
+  const [choice1, setChoice1] = useState(null)
+  const [choice2, setChoice2] = useState(null)
+  const [numTurns, setNumTurns] = useState(0);
+
+  function onStartClick() {
+    console.log("new game button Pressed");
+  }
+
+  function checkMatch(card1,card2) {
+    return (card1 === card2)
+  }
+
+  function calculateWinner(cards) {
+    for (let i = 0; i < cards.length; i++){
+      if (cards.isMatched === false) {
+        return false; //game is not over yet because we have a card that isn't matched
+      }
+    }
+    return true;
+  }
+
+  function handleClick(i) {
+    const nextCards = cards.slice();
+    const cardClicked = nextCards[i]
+    if (choice1){
+      if (cardClicked.isFacedDown && cardClicked.isMatched === false){
+        setChoice2(cardClicked.frontImage);
+        console.log("choice 1 is ", choice1, " and choice 2 is: ", cardClicked.frontImage);
+      
+        [cardClicked.backImage, cardClicked.frontImage] = [cardClicked.frontImage, cardClicked.backImage];
+        cardClicked.isFacedDown = false;
+        setCards(nextCards);  //update the cards to reflect picked card
+        setNumTurns(numTurns+1);
+        
+        checkMatch(choice1, cardClicked.frontImage);
+        //use setTimeout() here
+        setChoice1(null);
+        setChoice2(null);
+      }
+      checkMatch(choice1, cardClicked.frontImage);
+    }
+    if (!choice1){  //if we dont have choice 1, pick choice1 and set choice 1 only if the card is faced down and not matched
+      if (cardClicked.isFacedDown && cardClicked.isMatched === false) {
+        setChoice1(cardClicked.frontImage); //set choice 1
+        [cardClicked.backImage, cardClicked.frontImage] = [cardClicked.frontImage, cardClicked.backImage] //flip card
+        cardClicked.isFacedDown = false;  //change card to be faced up
+        setCards(nextCards);  //update the cards to reflect picked card
+      }
+    }
+  }
+  return (
+    <>
+    <div className="title">Match the Team Logos</div>
+    <div className="start-button">
+      <button onClick={onStartClick}>Start New Game</button>
+    </div>
+    <div className="board-row">
+      <Card frontImage={cards[0].frontImage} backImage={cards[0].backImage} isFacedDown={cards[0].isFacedDown} onCardClick={() => handleClick(0)} />
+      <Card frontImage={cards[1].frontImage} backImage={cards[1].backImage} isFacedDown={cards[1].isFacedDown} onCardClick={() => handleClick(1)} />
+      <Card frontImage={cards[2].frontImage} backImage={cards[2].backImage} isFacedDown={cards[2].isFacedDown} onCardClick={() => handleClick(2)} />
+      <Card frontImage={cards[3].frontImage} backImage={cards[3].backImage} isFacedDown={cards[3].isFacedDown} onCardClick={() => handleClick(3)} />
+    </div>
+    <div className="board-row">
+      <Card frontImage={cards[4].frontImage} backImage={cards[4].backImage} isFacedDown={cards[4].isFacedDown} onCardClick={() => handleClick(4)} />
+      <Card frontImage={cards[5].frontImage} backImage={cards[5].backImage} isFacedDown={cards[5].isFacedDown} onCardClick={() => handleClick(5)} />
+      <Card frontImage={cards[6].frontImage} backImage={cards[6].backImage} isFacedDown={cards[6].isFacedDown} onCardClick={() => handleClick(6)} />
+      <Card frontImage={cards[7].frontImage} backImage={cards[7].backImage} isFacedDown={cards[7].isFacedDown} onCardClick={() => handleClick(7)} />
+    </div>
+    <div className="board-row">
+      <Card frontImage={cards[8].frontImage} backImage={cards[8].backImage} isFacedDown={cards[8].isFacedDown} onCardClick={() => handleClick(8)} />
+      <Card frontImage={cards[9].frontImage} backImage={cards[9].backImage} isFacedDown={cards[9].isFacedDown} onCardClick={() => handleClick(9)} />
+      <Card frontImage={cards[10].frontImage} backImage={cards[10].backImage} isFacedDown={cards[10].isFacedDown} onCardClick={() => handleClick(10)} />
+      <Card frontImage={cards[11].frontImage} backImage={cards[11].backImage} isFacedDown={cards[11].isFacedDown} onCardClick={() => handleClick(11)} />
+    </div>
+    <div className="status">Number of turns: {numTurns} </div>
+    </>
+  );
+}
+
+function Card({ frontImage, backImage, isFacedDown, onCardClick }) {
+  let imageSource = ""
+  imageSource = backImage
+  return (
+    <img 
+      className="card" 
+      src={imageSource} 
+      alt="mlb logo"
+      onClick={onCardClick}
+    />
+  )
+}
+
+
+
